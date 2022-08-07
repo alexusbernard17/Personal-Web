a70fca0bd325be2753279caafc60059c70c8cd63
alexusbernard17 <alexusbernardbrooklyn@gmail.com>
2022-08-06 22:05:07 -0400

    Updates

---
+++ precache-manifest.c59467292a2139c06839c0b4f5c012fc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38ca52befb35c4ef9bf8a31be8b04919",
+    "url": "/Personal-Web/index.html"
+  },
+  {
+    "revision": "6eac9571bde0d4b5ea6f",
+    "url": "/Personal-Web/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "f317b9cf3109f877b4f5",
+    "url": "/Personal-Web/static/css/main.4b4228c5.chunk.css"
+  },
+  {
+    "revision": "6eac9571bde0d4b5ea6f",
+    "url": "/Personal-Web/static/js/2.b957805f.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/Personal-Web/static/js/2.b957805f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f317b9cf3109f877b4f5",
+    "url": "/Personal-Web/static/js/main.559a36ee.chunk.js"
+  },
+  {
+    "revision": "660e87d7c718a8446566",
+    "url": "/Personal-Web/static/js/runtime-main.dd6fa0a8.js"
+  },
+  {
+    "revision": "e9bd2dee4712b6743a79591c3e41e221",
+    "url": "/Personal-Web/static/media/GoldenSignature.e9bd2dee.otf"
+  }
+]);
